446da470255bbf4b951f797e52c01dc60f9678cc
Shashank358 <srshahare22@gmail.com>
2020-06-01 15:28:04 +0530

    Updates

---
+++ precache-manifest.14fe94d134bc4c538d1104d2968ed445.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb49d88cd93d931e5cd3008dd098bc28",
+    "url": "/cribe-client/index.html"
+  },
+  {
+    "revision": "db038dd6a1fc476acfde",
+    "url": "/cribe-client/static/css/main.972ca87a.chunk.css"
+  },
+  {
+    "revision": "27ed9317309333586b45",
+    "url": "/cribe-client/static/js/2.4cf98ddf.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/cribe-client/static/js/2.4cf98ddf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db038dd6a1fc476acfde",
+    "url": "/cribe-client/static/js/main.fa3baa3c.chunk.js"
+  },
+  {
+    "revision": "98454a0f5cee15f7a47a",
+    "url": "/cribe-client/static/js/runtime-main.21a5328b.js"
+  },
+  {
+    "revision": "238e3bbff8a5f5833b309c62aa4efb42",
+    "url": "/cribe-client/static/media/community.238e3bbf.svg"
+  },
+  {
+    "revision": "0e07bad6c3eced70d905310d41f407ae",
+    "url": "/cribe-client/static/media/earnmoney.0e07bad6.svg"
+  },
+  {
+    "revision": "df83de9b7c632a27dbf3cdeb78c0bac7",
+    "url": "/cribe-client/static/media/firstsection.df83de9b.svg"
+  },
+  {
+    "revision": "b0842109db308e206703a0ef1535f3c6",
+    "url": "/cribe-client/static/media/hashtagback.b0842109.jpg"
+  },
+  {
+    "revision": "144b8e7d709b5441efe48bdbcb8508bb",
+    "url": "/cribe-client/static/media/login.144b8e7d.svg"
+  },
+  {
+    "revision": "a52ad3241a8881be636072f83ce13e3d",
+    "url": "/cribe-client/static/media/thirdsection.a52ad324.svg"
+  }
+]);
