d53d1813ca50ced0ed6d5168d3357e51f5060de0
Shashank358 <srshahare22@gmail.com>
2020-06-22 12:56:03 +0530

    Updates

---
+++ precache-manifest.d339fbd97fa1097a500d4773f8a40e96.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d2edea2fe986b9c60f5ceebcce2af58",
+    "url": "/cribe-client/index.html"
+  },
+  {
+    "revision": "ad490cb49e715fb9f21d",
+    "url": "/cribe-client/static/css/2.d7c45ada.chunk.css"
+  },
+  {
+    "revision": "20bcafdd8f9678d311b3",
+    "url": "/cribe-client/static/css/main.15c45a8c.chunk.css"
+  },
+  {
+    "revision": "ad490cb49e715fb9f21d",
+    "url": "/cribe-client/static/js/2.1bfe657b.chunk.js"
+  },
+  {
+    "revision": "1d9559dc1d9c41936c1cdb350b43dcf9",
+    "url": "/cribe-client/static/js/2.1bfe657b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20bcafdd8f9678d311b3",
+    "url": "/cribe-client/static/js/main.df9ee436.chunk.js"
+  },
+  {
+    "revision": "98454a0f5cee15f7a47a",
+    "url": "/cribe-client/static/js/runtime-main.21a5328b.js"
+  },
+  {
+    "revision": "238e3bbff8a5f5833b309c62aa4efb42",
+    "url": "/cribe-client/static/media/community.238e3bbf.svg"
+  },
+  {
+    "revision": "0e07bad6c3eced70d905310d41f407ae",
+    "url": "/cribe-client/static/media/earnmoney.0e07bad6.svg"
+  },
+  {
+    "revision": "df83de9b7c632a27dbf3cdeb78c0bac7",
+    "url": "/cribe-client/static/media/firstsection.df83de9b.svg"
+  },
+  {
+    "revision": "b0842109db308e206703a0ef1535f3c6",
+    "url": "/cribe-client/static/media/hashtagback.b0842109.jpg"
+  },
+  {
+    "revision": "144b8e7d709b5441efe48bdbcb8508bb",
+    "url": "/cribe-client/static/media/login.144b8e7d.svg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/cribe-client/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/cribe-client/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/cribe-client/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "a52ad3241a8881be636072f83ce13e3d",
+    "url": "/cribe-client/static/media/thirdsection.a52ad324.svg"
+  }
+]);
